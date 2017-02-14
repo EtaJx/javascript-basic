6a34f4f26ab59d1fc2cc7bfc60e412291dd92613
Hing <youyaoguai@outlook.com>
2017-02-14 10:49:34 +0800

    beautiful

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -3,61 +3,62 @@
  */
 
 
- 
- function ajax(params) {
-     params = params || {};
-     params.data = params.data || {};
-     var json = params.jsonp ? jsonp(params) : json(params);
-     function json(params){
-         params.type = (params.method || 'GET').toUpperCase();
-         params.data = formatParams(params.data);
-         var xhr = null;
-         if(window.XMLHttpRequest){
-             xhr = new XMLHttpRequest();
-         }else{
-             xhr = new ActiveXObject('Micorosoft.XMLHTTP');
-         }
+function ajax(params) {
+    params = params || {};
+    params.data = params.data || {};
+    var json = params.jsonp ? jsonp(params) : json(params);
 
-         xhr.onreadystatechange = function(){
-             if(xhr.readyState == 4){
-                 var status = xhr.status;
-                 if(status >= 200 && status < 300){
-                     var response = '';
-                     var type = xhr.getResponseHeader('content-type');
-                     if(type.indexOf('xml') !== -1 && xhr.responseXML){
-                         response = xhr.responseXML;
-                     }else if(type === 'application/json'){
-                         response = JSON.parse(xhr.responseText);
-                     }else{
-                         reponse = xhr.reponseText;
-                     }
-                     params.success && params.success(response);
-                 }else{
-                     params.error && params.error(status);
-                 }
-             }
-         };
+    function json(params) {
+        params.type = (params.method || 'GET').toUpperCase();
+        params.data = formatParams(params.data);
+        var xhr = null;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject('Micorosoft.XMLHTTP');
+        }
 
-         if(prams.type == 'GET'){
-             xhr.open(params.type,params.url+'?'+params.data,true);
-             xhr.send(null);
-         } else {
-             xhr.open(params.type,params.url,true);
-             xhr.setRequestHeader('content-type','application/x-www-form-urlencoded;charset=UTF-8');
-             xhr.send(params.data);
-         }
-     }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var status = xhr.status;
+                if (status >= 200 && status < 300) {
+                    var response = '';
+                    var type = xhr.getResponseHeader('content-type');
+                    if (type.indexOf('xml') !== -1 && xhr.responseXML) {
+                        response = xhr.responseXML;
+                    } else if (type === 'application/json') {
+                        response = JSON.parse(xhr.responseText);
+                    } else {
+                        reponse = xhr.reponseText;
+                    }
+                    params.success && params.success(response);
+                } else {
+                    params.error && params.error(status);
+                }
+            }
+        };
 
-     function formatParams(data){
-         var arr = [];
-         for (var name in data){
-             arr.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-         }
-         arr.push('v=' + random());
-         return  arr.join('&');
-     }
-     function random(){
-         return Math.floor(Math.random()*10000+500);
-     }
+        if (prams.type == 'GET') {
+            xhr.open(params.type, params.url + '?' + params.data, true);
+            xhr.send(null);
+        } else {
+            xhr.open(params.type, params.url, true);
+            xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+            xhr.send(params.data);
+        }
+    }
 
- }
+    function formatParams(data) {
+        var arr = [];
+        for (var name in data) {
+            arr.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+        }
+        arr.push('v=' + random());
+        return arr.join('&');
+    }
+
+    function random() {
+        return Math.floor(Math.random() * 10000 + 500);
+    }
+
+}
